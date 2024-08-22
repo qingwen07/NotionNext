c5100df7c8dd47babef2e356df960b57f446a9cf
qingwen07 <echo.qingwen@gmail.com>
2024-08-22 12:25:42 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到大熊分享资源站', url: '#' },
+    { title: '点我及时获取最新有用、有趣的资源', url: 'https://t.me/daxiongshare' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -15,12 +15,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '分享云盘资源',
+  HEO_HERO_TITLE_2: '与视频课程',
+  HEO_HERO_TITLE_3: 'bigbear.com',
+  HEO_HERO_TITLE_4: '资源推荐',
+  HEO_HERO_TITLE_5: '最新推荐资源',
+  HEO_HERO_TITLE_LINK: '#',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
